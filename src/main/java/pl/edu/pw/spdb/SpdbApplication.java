8b18e2b2651a698175f85ae16109d9b8b737033f
p-karbownik <baciar112@gmail.com>
2022-12-23 16:37:00 +0100

    initial commit

---
+++ src/main/java/pl/edu/pw/spdb/SpdbApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.spdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pw/spdb/SpdbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.spdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
