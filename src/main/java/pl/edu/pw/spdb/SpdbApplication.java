58d6b4c62b8992fb6fcca572bbb7222d8fc157f5
p-karbownik <baciar112@gmail.com>
2022-12-25 15:44:03 +0100

    integrated javafx with spring

--- src/main/java/pl/edu/pw/spdb/SpdbApplication.java
+++ src/main/java/pl/edu/pw/spdb/SpdbApplication.java
@@ -1,25 +1,13 @@
 package pl.edu.pw.spdb;
 
+
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-public class SpdbApplication extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+@SpringBootApplication
+public class SpdbApplication {
 
     public static void main(String[] args) {
-        launch();
+        Application.launch(ClientUI.class, args);
     }
 }
