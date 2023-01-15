61dc454c4da48b83d8b5e9ee8277918f18e287df
AMazur2 <arturmazur18@gmail.com>
2023-01-15 15:34:54 +0100

    Add descriptions for functions Add targetCompatibility in build.gradle

--- src/main/java/pl/edu/pw/spdb/dal/DatabaseServiceImpl.java
+++ src/main/java/pl/edu/pw/spdb/dal/DatabaseServiceImpl.java
@@ -1,6 +1,7 @@
 package pl.edu.pw.spdb.dal;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.edu.pw.spdb.model.Point;
@@ -30,6 +31,16 @@ public class DatabaseServiceImpl implements DatabaseService {
     @Value("${db.password}")
     private String DB_PASSWORD;
 
+    /***
+     * Funkcja która przygotowuje zapytanie pod wyszukanie nakrótszej trasy - zamienia znaki zapytania z FIND_ROUTE_SQL
+     * na odpowiednie wartości podane przy jej wywołaniu
+     * @param startId - id punktu startu
+     * @param endId - id punktu końcowego
+     * @param maxSpeed - maksymalna prędkość pojazdu
+     * @param distanceWeight - waga wpływająca na liczenie kosztu wyszukania
+     * @param connection - połączenie z baż
+     * @return PreparedStatement
+     */
     private static PreparedStatement getStatement(long startId, long endId, Integer maxSpeed, float distanceWeight, Connection connection) throws SQLException {
         PreparedStatement statement = connection.prepareStatement(FIND_ROUTE_SQL);
         statement.setLong(1, startId);
@@ -39,6 +50,12 @@ public class DatabaseServiceImpl implements DatabaseService {
         return statement;
     }
 
+    /***
+     * Funkcja parsuje jeden rekord z ResultSet otrzymanego po wykonaniu zapytania FIND_ROTE_SQL na RouteSegment -
+     * wyciąga porzebne informacje oraz oblicza długość segmentu w km
+     * @param result - rekord otrzymany z zaytania
+     * @return RouteSegment
+     */
     private static RouteSegment parseRecord(ResultSet result) throws SQLException {
         long id = result.getLong(1);
         Object geom = result.getObject(2);
@@ -54,12 +71,23 @@ public class DatabaseServiceImpl implements DatabaseService {
         return new RouteSegment(id, geom, source, target, length / 1000, maxSpeedForward, x1, y1, x2, y2);
     }
 
+    /***
+     * Funkcja zwraca połączenie z bazą danych
+     * @return java.sql.Connection
+     */
     private Connection getConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         connection.setAutoCommit(true);
         return connection;
     }
 
+    /***
+     * Funkcja wyszukująca początek lub koniec trasy na podstawie punktu, parametr isStartPoin pozwala nam określić
+     * którego zapytania powinniśmy użyć - NEAREST_START_ID_SQL czy NEAREST_END_ID_SQL
+     * @param point - punkt wbrany w GUI po którym  wyszukujemy najbliższego początku trasy
+     * @param isStartPoint - parametr pomagający stwierdzić którego zapytania należy użyć
+     * @return id punktu w bazie
+     */
     @Override
     public Long getStartOrEnd(Point point, boolean isStartPoint) throws RuntimeException {
         try (Connection connection = getConnection()) {
@@ -85,6 +113,14 @@ public class DatabaseServiceImpl implements DatabaseService {
         }
     }
 
+    /***
+     * Funkcja wyszukująca najlepszą trasę dla zadanych parametrów przy użyciu zdefiniowanego wsześniej zapytania FIND_ROUTE_SQL
+     * @param startId - id punktu początkowego
+     * @param endId - id punktu końcowego
+     * @param maxSpeed - maksymalna prędkość pojazdu
+     * @param distanceWeight - waga wpływająca na liczenie kosztu wyszukania
+     * @return Route - znaleziona ścieżka
+     */
     @Override
     public Route findRoute(long startId, long endId, Integer maxSpeed, float distanceWeight) throws RuntimeException {
         try (Connection connection = getConnection()) {
@@ -102,7 +138,13 @@ public class DatabaseServiceImpl implements DatabaseService {
         }
     }
 
-    private ResultSet executeStatement(PreparedStatement statement) throws SQLException {
+    /***
+     * Funkcja wykonująca przygotowane zapytanie - jej zadaniem jest także zmierzenie czasu potrzebnego do wykonania
+     * okreslonego zapytania
+     * @param statement - przygotowane zapytanie
+     * @return ResultSet - wynik wykonania zapytania SQL
+     */
+    private ResultSet executeStatement(@NotNull PreparedStatement statement) throws SQLException {
         long start = System.currentTimeMillis();
         ResultSet result = statement.executeQuery();
         long end = System.currentTimeMillis();
@@ -112,6 +154,12 @@ public class DatabaseServiceImpl implements DatabaseService {
         return result;
     }
 
+    /***
+     * Funkcja której zadaniem jest przeparsować otrzymane wyniki z wykonania zapytania FIND_ROUTE_SQL
+     * @param result - wynik otrzymany z bazy
+     * @param maxSpeed - maksymalna prędkość pojazdu, potrzebna do wyliczenia czasu potrzebnego na pokonanie odcinka drogi
+     * @return Route - klasa zawierająca informacje o znalezionej trasie
+     */
     private Route parseQueryResult(ResultSet result, int maxSpeed) throws SQLException {
         List<RouteSegment> segments = new ArrayList<>();
 
